fe29d01860cd8a5fc35f7b304ada2bbf361ffe98
celinaLind <clind323@gmail.com>
2024-11-08 20:50:51 -0800

    add dashboard, shadcn, and sidebar

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,112 @@
+"use client";
+import { Avatar } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupAction,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+} from "@/components/ui/sidebar";
+import { api } from "@/convex/_generated/api";
+import { RedirectToSignIn, SignOutButton } from "@clerk/nextjs";
+import { AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import {
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "@radix-ui/react-dropdown-menu";
+import { Authenticated, Unauthenticated, useQuery } from "convex/react";
+import { PlusIcon, User2Icon } from "lucide-react";
+import Link from "next/link";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Authenticated>
+        <SidebarProvider>
+          <DashboardSidebar />
+          {children}
+        </SidebarProvider>
+      </Authenticated>
+      <Unauthenticated>
+        <RedirectToSignIn />
+      </Unauthenticated>
+    </>
+  );
+}
+
+function DashboardSidebar() {
+  const user = useQuery(api.functions.user.get);
+
+  if (!user) {
+    return null;
+  }
+
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <Link href="/friends">
+                    <User2Icon />
+                    Friends
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        <SidebarGroup>
+          <SidebarGroupLabel>DirectMessages</SidebarGroupLabel>
+          <SidebarGroupAction>
+            <PlusIcon />
+            <span className="sr-only">New Direct Message</span>
+          </SidebarGroupAction>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <DropdownMenu>
+                  {/* asChild means the sidebarmenubutton will be the dropdown menu trigger */}
+                  <DropdownMenuTrigger asChild>
+                    <SidebarMenuButton className="flex items-center">
+                      <Avatar className="size-6">
+                        <AvatarImage src={user!.image} />
+                        <AvatarFallback>{user.username[0]}</AvatarFallback>
+                      </Avatar>
+                      <p className="font-medium">{user.username}</p>
+                    </SidebarMenuButton>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                    <DropdownMenuItem asChild>
+                      <SignOutButton />
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
